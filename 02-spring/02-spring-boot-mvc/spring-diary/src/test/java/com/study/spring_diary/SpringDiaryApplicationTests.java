b4fad80b3fcaad99cf33ff3da5fddd36fbc4988e
R1NN <r1nn650t@gmail.com>
2026-06-24 13:12:54 +0900

    code(spring): add spring diary MVC project

---
+++ 02-spring/02-spring-boot-mvc/spring-diary/src/test/java/com/study/spring_diary/SpringDiaryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.study.spring_diary;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringDiaryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
